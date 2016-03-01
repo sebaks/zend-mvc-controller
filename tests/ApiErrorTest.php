ee14f0a644dc85e5c6cda6f8a0e028b3ec98d613
maxgu <gulturyan@t4web.com.ua>
2016-03-01 12:06:22 +0200

    add HtmlError test + add ApiError test

---
+++ tests/ApiErrorTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Sebaks\ZendMvcControllerTest;
+
+use Sebaks\ZendMvcController\ApiError;
+
+class ApiErrorTest extends \PHPUnit_Framework_TestCase
+{
+    private $error;
+    private $event;
+
+    public function setUp()
+    {
+        $this->event = $this->prophesize('Zend\Mvc\MvcEvent');
+
+        $this->error = new ApiError(
+            $this->event->reveal()
+        );
+    }
+
+    public function testMethodNotAllowed()
+    {
+        $zendResponse = $this->prophesize('Zend\Http\Response');
+        $zendResponse->setStatusCode(405)->willReturn(null);
+
+        $this->event->getResponse()->willReturn($zendResponse->reveal());
+
+        $this->error->methodNotAllowed();
+    }
+
+    public function testNotFoundByRequestedCriteria()
+    {
+        $zendResponse = $this->prophesize('Zend\Http\Response');
+        $zendResponse->setStatusCode(404)->willReturn(null);
+
+        $this->event->getResponse()->willReturn($zendResponse->reveal());
+
+        $this->error->notFoundByRequestedCriteria();
+    }
+}
