8d2a9587f2e391b538f502cf0fad8499e7313a66
maxgu <gulturyan@t4web.com.ua>
2016-03-10 10:52:47 +0200

    create Module.php

---
+++ src/Module.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sebaks\ZendMvcController;
+
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements ConfigProviderInterface
+{
+    public function getConfig($env = null)
+    {
+        return include dirname(__DIR__) . '/../config/module.config.php';
+    }
+}
